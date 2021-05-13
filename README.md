8d0e4c66c39ca2899304c00fe53c690c50894545
Trauvin <mateusgalote@gmail.com>
2021-05-13 11:14:05 -0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# finding-and-list
---
+++ dir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Tests the directory, if there is a return
+#XXX I want to prevent the user from entering 
+# the entire path /home/$USER
+# Enter search directory only
+# Ex: 
+# 	Enter the directory:
+# 	Downloads
+# 	Directory found: /home/user/Downloads
+
+echo "Enter search directory: "
+read dir
+# Base directory
+base_dir="/home/$USER"
+
+# To use absolute paths,remove that line
+dir="$base_dir/$dir"
+
+if test -d  "$dir"
+then
+	echo -e '\033[32m Found directory: \033[m'"$dir \n"
+	ls $dir
+else
+	echo -e '\033[31m Directory not found!\033[m'
+fi
